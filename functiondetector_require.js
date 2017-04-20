58be21c967cab92b4259a6b25977b671209eacb2
narekhovhannisyan <narekgilmour@gmail.com>
2017-04-20 15:47:14 +0400

    fixed content

--- functiondetector_require.js
+++ functiondetector_require.js
@@ -1,24 +1,2 @@
-
-//var functiondetector = require('./functiondetector.js')
-//functiondetector(add().)
-
-function counter() {
-  var current = 0
-  function counter() {
-    return current++
-  }
-  counter.set = function (yourNumber) {
-      current = yourNumber
-    }
-  counter.reset = function () {
-      current = 1
-    }
-    return counter;
-}
-
-
-var counter = counter()
-
-console.log(counter())
-console.log(counter.set(5))
-console.log(counter())
+var functiondetector = require('./functiondetector.js')
+functiondetector(add().)
